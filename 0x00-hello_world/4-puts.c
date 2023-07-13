ae33abd8b31adf327100228fd3c0a9555ff13fc5
zak-github <elmyar.zakaria@gmail.com>
2023-07-13 10:46:18 -0700

    task god

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /* This is the main function.
-  * It is the entry point of the program. */
+  * It is the entry point of the program.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
