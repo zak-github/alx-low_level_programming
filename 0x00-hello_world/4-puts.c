a304806f0e64bcf8aea8062c96c1508675ba6f9e
zak-github <elmyar.zakaria@gmail.com>
2023-07-13 10:34:50 -0700

    task 6 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main(){
-
-puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
-
+//
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
